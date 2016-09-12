0bfb2e3aa79461301be4f2ddb68555688e325abb
sudatta0993 <sudatta.mohanty@berkeley.edu>
2016-09-12 13:12:10 -0700

    Changes in summary_stat.py in response to comments

--- summary_stat.py
+++ summary_stat.py
@@ -4,33 +4,33 @@ def load_dataset(filename, skiprows, usecols):
     data = pd.read_csv(filename,skiprows=skiprows, usecols=usecols)
     data.columns = ['col1', 'col2', 'col3', 'average']
     filtered_data = pd.DataFrame(columns=['col1', 'col2', 'col3', 'average'])
+    complete = 0
     for index, row in data.iterrows():
         try:
-            filtered_data = filtered_data.append({'col1':int(row['col1']), 'col2':int(row['col2']), 'col3':int(row['col3']), 'average':float(row['average'])},ignore_index=True)
+            max_value = max(int(row['col1']), int(row['col2']), int(row['col3']))
+            min_value = min(int(row['col1']), int(row['col2']), int(row['col3']))
+            if (max_value - min_value >= 2):
+                filtered_data = filtered_data.append({'col1':int(row['col1']), 'col2':int(row['col2']), 'col3':int(row['col3']), 'average':float(row['average'])},ignore_index=True)
+                complete = complete + 1
         except ValueError:
             pass
-    return filtered_data
+    return (filtered_data, complete)
 
-def num_complete(data, col1, col2, col3, average):
-    complete = 0
+def mean_in_triplet(data, col1, col2, col3, average):
     total = 0
     contains_mean = 0
     for index, row in data.iterrows():
         list_values = (int(row[col1]), int(row[col2]), int(row[col3]))
         mean = int(round(int(row[average])))
-        max_value = max(int(row[col1]),int(row[col2]), int(row[col3]))
-        min_value = min(int(row[col1]),int(row[col2]), int(row[col3]))
-        if (max_value - min_value >= 2):
-            complete+=1
-            if (mean in list_values):
-                contains_mean += 1
+        if (mean in list_values):
+            contains_mean += 1
         total+=1
-    return (complete, total, contains_mean)
+    return (total, contains_mean)
 
 def run(file_location, skiprows, usecols):
-    data = load_dataset(file_location, skiprows, usecols)
+    (data, complete) = load_dataset(file_location, skiprows, usecols)
     data.columns = ['col1','col2','col3','average']
-    (complete, total, contains_mean) = num_complete(data, 'col1', 'col2','col3', 'average')
+    (total, contains_mean) = mean_in_triplet(data, 'col1', 'col2','col3', 'average')
     return (complete, total, contains_mean)
 
 if __name__ == "__main__":
