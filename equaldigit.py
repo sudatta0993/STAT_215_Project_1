57f914960dc2c2019690548caebb52e4f4da71f5
Mengyu Li <Gabrielle@calvisitor-10-105-189-49.calvisitor.1918.berkeley.edu>
2016-09-13 15:01:38 -0700

    filter difference < 1 triples

--- equaldigit.py
+++ equaldigit.py
@@ -5,35 +5,40 @@ def load_data(filename, skiprows, usecols):
     data.columns = ['col1', 'col2', 'col3']
     filtered_data = pd.DataFrame(columns=['col1', 'col2', 'col3'])
     complete = 0
+    total = 0
     for index, row in data.iterrows():
         try:
-            filtered_data = filtered_data.append({'col1': int(row['col1']), 'col2': int(row['col2']), 'col3': int(row['col3'])}, ignore_index=True)
-            complete = complete + 1
+            max_value = max(int(row['col1']), int(row['col2']), int(row['col3']))
+            min_value = min(int(row['col1']), int(row['col2']), int(row['col3']))
+            if (max_value - min_value >= 2):
+                filtered_data = filtered_data.append({'col1':int(row['col1']), 'col2':int(row['col2']), 'col3':int(row['col3'])},ignore_index=True)
+                complete = complete + 1
+            total = total + 1
         except ValueError:
             pass
-    return (filtered_data, complete)
+    return (filtered_data, complete, total)
 
 def equal_digit(filename, skiprows, usecols):
-    (data, complete) = load_data(filename, skiprows, usecols)
-    total = 0
+    (data, complete, total) = load_data(filename, skiprows, usecols)
+    greater = 0
     equal = 0
     for index, row in data.iterrows():
         if row['col1'] >= 100:
-            total = total + 1
+            greater = greater + 1
             digit_list = list(str(int(row['col1'])))
             if digit_list[len(digit_list) - 1] == digit_list[len(digit_list) - 2]:
                 equal = equal + 1
         if row['col2'] >= 100:
-            total = total + 1
+            greater = greater + 1
             digit_list = list(str(int(row['col2'])))
             if digit_list[len(digit_list) - 1] == digit_list[len(digit_list) - 2]:
                 equal = equal + 1
         if row['col3'] >= 100:
-            total = total + 1
+            greater = greater + 1
             digit_list = list(str(int(row['col3'])))
             if digit_list[len(digit_list) - 1] == digit_list[len(digit_list) - 2]:
                 equal = equal + 1
-    return total, equal
+    return greater, equal
 
 
 if __name__ == "__main__":
