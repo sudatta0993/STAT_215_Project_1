f18f564de2c57be689d2ff7c6ccf89573ae150b5
Mengyu Li <Gabrielle@Mengyus-MacBook-Pro.local>
2016-09-12 22:49:38 -0700

    equal digit analysis

--- equaldigit.py
+++ equaldigit.py
@@ -33,7 +33,6 @@ def equal_digit(filename, skiprows, usecols):
             digit_list = list(str(int(row['col3'])))
             if digit_list[len(digit_list) - 1] == digit_list[len(digit_list) - 2]:
                 equal = equal + 1
-
     return total, equal
 
 
@@ -41,17 +40,13 @@ if __name__ == "__main__":
 
     (RTStotal, RTSequal) = equal_digit('./data/OSF_Storage/Bishayee Coulter Counts.10.20.97-7.16.01.csv', 1,
                ["Count 1", "Count 2", "Count 3"])
-
     (otherTotal, otherEqual) = equal_digit('./data/OSF_Storage/Other Investigators in Lab.Coulter Counts.4.15.92-5.21.05.csv', 1,
                 ["Coul 1", "Coul 2", "Coul 3"])
-
     (outside2Total, outside2Equal) = equal_digit('./data/OSF_Storage/Outside Lab 2.Coulter Counts.6.6.08-7.7.08.csv', 1,
                ["Count 1", "Count 2", "Count 3"])
-
     (outside3Total, outside3Equal) = equal_digit('./data/OSF_Storage/Outside Lab 3.Colony Counts.2.4.10-5.21.12.csv', 1, ["c1", "c2", "c3"])
 
     print("RTS coulter (equal, total) = ", RTSequal, RTStotal)
     print("Others coulter (equal, total) = ", otherEqual + outside2Equal + outside3Equal, otherTotal + outside2Total + outside3Total)
-
     print ('equal digit percentage of RTS coulter = ', RTSequal / (RTStotal * 1.0))
     print ('equal digit percentage of others coulter = ', (otherEqual + outside2Equal + outside3Equal) / ((otherTotal + outside2Total + outside3Total) * 1.0))
