93b306c0fe88e8df37defce99789f23a7f5584bd
tmaurer13 <tmaurer13@berkeley.edu>
2016-09-16 16:55:21 -0700

    Changed some of the code to reflect this morning's discussion section; outputs should be the same

--- ChiSq.py
+++ ChiSq.py
@@ -7,16 +7,22 @@ import scipy.stats
 import pandas as pd
 from summary_stat import load_dataset
 
-# Define function to parse data needed for a chi squared goodness of fit test
-def chi_sq_data(data):
+# Define function to take only terminal digits of data
+def terminal_digit(num):
+	return int(num % 10)
+
+# Define function to shape data from authors into a vector
+def clean_data(data):
 	del data['average'] # Keep only count columns; discard average
+	vec_data = data.stack()
 
-	# Calculate observed frequency of each terminal digit
-	f_obs = list(0 for i in range(10))
+	return vec_data
 
-	for x in np.nditer(data):
-		end_digit = (int(x) % 10)
-		f_obs[end_digit] += 1
+# Calculate observed frequency of each terminal digit
+def term_digit_freq(vect):
+	end_digit = vect.apply(terminal_digit)
+	f_obs = np.bincount(end_digit)
+	
 	return f_obs
 
 # Define function that "manually" computes chi squared test statistic
@@ -32,8 +38,9 @@ def manual_chi_sq(f_obs):
 
 def run(filelocation, skiprows, usecols):
 	(data, total, complete) = load_dataset(filelocation, skiprows, usecols)
-	f_obs = chi_sq_data(data)
-
+	vec_data = clean_data(data)
+	f_obs = term_digit_freq(vec_data)
+	
 	# Calculate chi sq statistic via two methods + p-value using Python function
 	manual_chi2 = manual_chi_sq(f_obs)
 	chi2, p = scipy.stats.chisquare(f_obs) 
@@ -41,9 +48,9 @@ def run(filelocation, skiprows, usecols):
 
 if __name__ == "__main__":
 	print ("RTS colony (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Bishayee Colony Counts 10.27.97-3.8.01.csv', 2, ["col1","col2","col3","average"]))
-	print ("RTS coulter (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Bishayee Coulter Counts.10.20.97-7.16.01.csv', 1, ["Count 1", "Count 2", "Count 3", "Average"]))
-	print ("Others colony (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Other Investigators in Lab.Colony Counts.4.23.92-11.27.02.csv',1,["col1","col2","col3","average"]))
-	print ("Others coulter (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Other Investigators in Lab.Coulter Counts.4.15.92-5.21.05.csv',1,["Coul 1","Coul 2","Coul 3","Average"]))
-	print ("Outside lab 1 coulter (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Outside Lab 1.Coulter Counts.6.7.91-4.9.99.csv',0,[1,2,3,4]))
-	print ("Outside lab 2 coulter (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Outside Lab 2.Coulter Counts.6.6.08-7.7.08.csv',1,["Count 1", "Count 2", "Count 3", "Average"]))
-	print ("Outside lab 3 colony (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Outside Lab 3.Colony Counts.2.4.10-5.21.12.csv',1,["c1", "c2", "c3", "average"]))
+	# print ("RTS coulter (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Bishayee Coulter Counts.10.20.97-7.16.01.csv', 1, ["Count 1", "Count 2", "Count 3", "Average"]))
+	# print ("Others colony (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Other Investigators in Lab.Colony Counts.4.23.92-11.27.02.csv',1,["col1","col2","col3","average"]))
+	# print ("Others coulter (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Other Investigators in Lab.Coulter Counts.4.15.92-5.21.05.csv',1,["Coul 1","Coul 2","Coul 3","Average"]))
+	# print ("Outside lab 1 coulter (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Outside Lab 1.Coulter Counts.6.7.91-4.9.99.csv',0,[1,2,3,4]))
+	# print ("Outside lab 2 coulter (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Outside Lab 2.Coulter Counts.6.6.08-7.7.08.csv',1,["Count 1", "Count 2", "Count 3", "Average"]))
+	# print ("Outside lab 3 colony (Manual Chi Sq, Chi Sq, p-value, f_obs) = ", run('./data/OSF_Storage/Outside Lab 3.Colony Counts.2.4.10-5.21.12.csv',1,["c1", "c2", "c3", "average"]))
---
+++ Uniformity.py
@@ -0,0 +1,40 @@
+# File: Uniformity.py
+
+import sys
+import numpy as np
+import scipy
+import scipy.stats
+import pandas as pd
+import matplotlib.pyplot as plt
+from summary_stat import load_dataset
+from ChiSq import terminal_digit
+from ChiSq import term_digit_freq
+
+# Define function to test uniformity assumption
+def uniformity(data):
+	means = data['average'] # Use means as lambda values, as per author assumptions
+	s = np.random.poisson(lam = (means), size = (3,len(means))) # Generate random sets of triples
+	ran_poi_vect = pd.Series(np.reshape(s,s.size))
+
+	return ran_poi_vect
+
+def run(filelocation, skiprows, usecols):
+	(data, total, complete) = load_dataset(filelocation, skiprows, usecols)
+	ran_poi_vect = uniformity(data)
+	f_obs_test = term_digit_freq(ran_poi_vect)
+	
+	# plot histogram
+	plt.figure(figsize = (8,6))
+	plt.hist(f_obs_test, bins = np.arange(11))
+	plt.show
+	
+	return f_obs_test
+
+if __name__ == "__main__":
+	print ("RTS colony (f_obs_test) = ", run('./data/OSF_Storage/Bishayee Colony Counts 10.27.97-3.8.01.csv', 2, ["col1","col2","col3","average"]))
+	# print ("RTS coulter (f_obs_test) = ", run('./data/OSF_Storage/Bishayee Coulter Counts.10.20.97-7.16.01.csv', 1, ["Count 1", "Count 2", "Count 3", "Average"]))
+	# print ("Others colony (f_obs_test) = ", run('./data/OSF_Storage/Other Investigators in Lab.Colony Counts.4.23.92-11.27.02.csv',1,["col1","col2","col3","average"]))
+	# print ("Others coulter (f_obs_test) = ", run('./data/OSF_Storage/Other Investigators in Lab.Coulter Counts.4.15.92-5.21.05.csv',1,["Coul 1","Coul 2","Coul 3","Average"]))
+	# print ("Outside lab 1 coulter (f_obs_test) = ", run('./data/OSF_Storage/Outside Lab 1.Coulter Counts.6.7.91-4.9.99.csv',0,[1,2,3,4]))
+	# print ("Outside lab 2 coulter (f_obs_test) = ", run('./data/OSF_Storage/Outside Lab 2.Coulter Counts.6.6.08-7.7.08.csv',1,["Count 1", "Count 2", "Count 3", "Average"]))
+	# print ("Outside lab 3 colony (f_obs_test) = ", run('./data/OSF_Storage/Outside Lab 3.Colony Counts.2.4.10-5.21.12.csv',1,["c1", "c2", "c3", "average"]))
